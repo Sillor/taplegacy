484821942effc444259ca84f0664d028d95e7c4c
Egor <65542192+Sillor@users.noreply.github.com>
2023-12-06 16:52:58 -0800

    Adjust styling, code format

--- src/layout/Clicker.jsx
+++ src/layout/Clicker.jsx
@@ -1,16 +1,16 @@
+import React from 'react';
+
 function Clicker({ taps, addTaps, cps }) {
   return (
-    <div className="md:max-w-sm lg:max-w-lg md:h-[80vh] p-4 h-full w-full bg-black rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-60 border-2 border-white flex flex-col justify-center items-center py-36">
-      <h2 className="font-bold text-3xl md:text-4xl mb-16"> {taps} TAPS</h2>
+    <div className="flex flex-col items-center justify-center h-full p-4 md:max-w-sm lg:max-w-lg md:h-[80vh] w-full bg-black rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-60 border-2 border-white">
+      <h2 className="mb-16 text-3xl font-bold md:text-4xl">{taps} TAPS</h2>
       <button
-        className="p-6 text-5xl active:text-4xl font-extrabold border-2 border-white-400 backdrop-filter backdrop-blur-sm bg-white bg-opacity-5 rounded-xl tap--button"
-        onClick={() => {
-          addTaps();
-        }}
+        className="w-full p-6 text-5xl font-extrabold border-2 border-white-400 rounded-xl tap--button bg-white bg-opacity-5 backdrop-filter backdrop-blur-sm active:text-4xl h-24"
+        onClick={addTaps}
       >
         TAP!
       </button>
-      <p>{cps} taps / second</p>
+      <p className="mt-4">+{cps} taps / second</p>
     </div>
   );
 }
--- src/layout/Game.jsx
+++ src/layout/Game.jsx
@@ -1,35 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Upgrades from './Upgrades';
 import Clicker from './Clicker';
 
 function Game() {
-  const [taps, setTaps] = React.useState(0);
+  const [taps, setTaps] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [clicks, setClicks] = useState(0);
+  const [cps, setCPS] = useState(0);
 
-  const [seconds, setSeconds] = React.useState(0);
-  const [clicks, setClicks] = React.useState(0);
-  const [cps, setCPS] = React.useState(0);
+  const addTaps = () => {
+    setTaps((oldTaps) => oldTaps + 1);
+    setClicks((oldClicks) => oldClicks + 1);
+  };
 
-  function addTaps() {
-    setTaps((oldTaps) => (oldTaps += 1));
-    setClicks((oldClicks) => (oldClicks += 1));
-  }
-
-  React.useEffect(() => {
+  useEffect(() => {
     const interval = setInterval(() => {
-      setSeconds((oldSeconds) => oldSeconds + 1);
-    }, 1000);
+      setSeconds((oldSeconds) => oldSeconds + 0.1);
+    }, 100);
 
     return () => clearInterval(interval);
   }, []);
 
-  React.useEffect(() => {
-    if (seconds >= 2) {
-      setCPS(clicks / seconds);
+  useEffect(() => {
+    if (seconds >= 1) {
+      setCPS((clicks / seconds).toFixed(2));
       setSeconds(0);
       setClicks(0);
     }
     // console.log(`S: ${seconds}\tC: ${clicks}\tCPS: ${cps}\t`);
-  }, [seconds, clicks, cps]);
+  }, [seconds, clicks]);
 
   return (
     <div className="flex justify-center flex-wrap gap-4">
