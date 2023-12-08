fdcb8e65601bbbc3f0b621bcba9333ecf929fa5f
Egor <65542192+Sillor@users.noreply.github.com>
2023-12-07 16:02:22 -0800

    Add conditional render to upgrades

--- src/components/Upgrade.jsx
+++ src/components/Upgrade.jsx
@@ -2,34 +2,36 @@ function Upgrade({ name, price, styles, upgradeClicks, increase, userData }) {
   const isUpgradeBought = userData.upgrades[name] > 0;
 
   return (
-    <div
-      className={`mt-2 p-4 upgrade h-full w-full ${styles.bg} rounded-md bg-opacity-10 border ${styles.border} max-h-28`}
-    >
-      <h1 className={`font-bold text-md ${styles.text}`}>
-        {name[0].toUpperCase() + name.slice(1)} Upgrade
-      </h1>
-      <p className="text-sm font-light mb-2 italic">
-        +{increase.toLocaleString()} Taps per Second
-      </p>
-      <div className="flex justify-between items-center">
-        <p className="text-sm font-bold">
-          Price:{' '}
-          <span className={`${styles.text} font-normal`}>
-            {price.toLocaleString()} Taps
-          </span>
+    (userData.taps > price / 3 || isUpgradeBought) && (
+      <div
+        className={`mt-2 p-4 upgrade h-full w-full ${styles.bg} rounded-md bg-opacity-10 border ${styles.border} max-h-28`}
+      >
+        <h1 className={`font-bold text-md ${styles.text}`}>
+          {name[0].toUpperCase() + name.slice(1)} Upgrade
+        </h1>
+        <p className="text-sm font-light mb-2 italic">
+          +{increase.toLocaleString()} Taps per Second
         </p>
-        {isUpgradeBought ? (
-          <span className="text-sm font-bold text-green-500">Bought</span>
-        ) : (
-          <button
-            className="p-1 text-xs rounded border border-white bg-white bg-opacity-10"
-            onClick={() => upgradeClicks(increase, name, price)}
-          >
-            Upgrade!
-          </button>
-        )}
+        <div className="flex justify-between items-center">
+          <p className="text-sm font-bold">
+            Price:{' '}
+            <span className={`${styles.text} font-normal`}>
+              {price.toLocaleString()} Taps
+            </span>
+          </p>
+          {isUpgradeBought ? (
+            <span className="text-sm font-bold text-green-500">Bought</span>
+          ) : (
+            <button
+              className="p-1 text-xs rounded border border-white bg-white bg-opacity-10"
+              onClick={() => upgradeClicks(increase, name, price)}
+            >
+              Upgrade!
+            </button>
+          )}
+        </div>
       </div>
-    </div>
+    )
   );
 }
 
--- src/layout/Upgrades.jsx
+++ src/layout/Upgrades.jsx
@@ -53,66 +53,6 @@ function Upgrades({ upgradeClicks, userData }) {
     <div className="md:max-w-sm lg:max-w-lg md:h-[80vh] overflow-y-auto p-4 h-full w-full bg-black rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-60 border-2 border-white no-scrollbar">
       <h1 className="font-bold text-xl pb-2">Upgrades</h1>
       {upgElems}
-      {/* <Upgrade
-        userData={userData}
-        upgradeClicks={upgradeClicks}
-        name={'basic'}
-        price={10}
-        increase={3}
-        styles={{
-          bg: 'bg-green-400',
-          border: 'border-green-400',
-          text: 'text-green-400',
-        }}
-      />
-      <Upgrade
-        userData={userData}
-        upgradeClicks={upgradeClicks}
-        name={'rare'}
-        increase={10}
-        price={500}
-        styles={{
-          bg: 'bg-blue-400',
-          border: 'border-blue-400',
-          text: 'text-blue-400',
-        }}
-      />
-      <Upgrade
-        userData={userData}
-        upgradeClicks={upgradeClicks}
-        name={'epic'}
-        increase={50}
-        price={10000}
-        styles={{
-          bg: 'bg-purple-400',
-          border: 'border-purple-400',
-          text: 'text-purple-400',
-        }}
-      />
-      <Upgrade
-        userData={userData}
-        upgradeClicks={upgradeClicks}
-        name={'legendary'}
-        increase={1000}
-        price={50000}
-        styles={{
-          bg: 'bg-orange-400',
-          border: 'border-orange-400',
-          text: 'text-orange-400',
-        }}
-      />
-      <Upgrade
-        userData={userData}
-        upgradeClicks={upgradeClicks}
-        name={'mythic'}
-        increase={5000}
-        price={250000}
-        styles={{
-          bg: 'bg-pink-400',
-          border: 'border-pink-400',
-          text: 'text-pink-400',
-        }}
-      /> */}
     </div>
   );
 }
