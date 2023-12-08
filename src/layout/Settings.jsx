8ed177b365ffc8cf8461aa5b79d6192c6340bf60
Egor <65542192+Sillor@users.noreply.github.com>
2023-12-07 21:20:17 -0800

    Fix remove data

--- src/layout/Settings.jsx
+++ src/layout/Settings.jsx
@@ -1,6 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Settings() {
+  const navigate = useNavigate();
+
+  const handleRemoveData = () => {
+    localStorage.removeItem('user');
+    navigate('/');
+    location.reload();
+  };
+
   return (
     <div className="page-container flex justify-center items-center">
       <div className="md:max-w-[60vw] xl:max-w-3xl md:h-[80vh] overflow-y-auto p-4 md:px-6 h-full w-full bg-black rounded-md bg-clip-padding bg-opacity-60 border-2 border-white no-scrollbar flex flex-col items-start gap-4">
@@ -8,10 +16,7 @@ function Settings() {
 
         <button
           className="text-md font-extrabold border-2 bg-black bg-opacity-30 rounded-md p-2 text-red-600"
-          onClick={() => {
-            localStorage.removeItem('user');
-            location.reload();
-          }}
+          onClick={handleRemoveData}
         >
           Remove data
         </button>
