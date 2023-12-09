ea1b1a0698dabfd5a4f8dfadc15518fa0c0faaed
Egor <65542192+Sillor@users.noreply.github.com>
2023-12-08 16:39:36 -0800

    Add footer

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,4 +1,5 @@
 import Navbar from './Navbar';
+import Footer from './Footer';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
@@ -7,6 +8,7 @@ export default function Layout() {
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 }
